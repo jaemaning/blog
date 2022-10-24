71c769786c1598fb5562c5f9111e7cddfe22c014
김재만 <lovesay00@naver.com>
2022-10-25 01:15:54 +0900

    20221025_kjm

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = '블로그 내용'
-  let blog_name = 'black_id'
-  let [글제목, b] = useState(['남자 코트 추천', '재만 바보', '리액트 재밌다'])
-  let [글날짜, setDatetime] = useState(['2022-10-22', '2022-10-23', '2022-10-24'])
+  let post = '블로그 내용';
+  let blog_name = 'black_id';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '재만 바보', '리액트 재밌다']);
+  let [글날짜, setDatetime] = useState(['2022-10-22', '2022-10-23', '2022-10-24']);
+  let [like_count, setLikecount] = useState(0);
+
+  // function like_click() {
+  //   like_count += 1
+  // }
 
   let [a, c] = [1, 2];
   // let num = [1,2]
@@ -20,19 +25,34 @@ function App() {
         <h4 id={blog_name}>Jaeman blog</h4>
       </div>
       <div className='list'>
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]} <span onClick={() => { setLikecount(like_count + 1) }}>좋아요</span> {like_count} </h4>
+
+        <button onClick={() => {
+          let copy글제목 = [...글제목]
+          copy글제목.sort()
+          글제목변경(copy글제목)
+        }
+        }>정렬</button>
+
+        <button onClick={() => {
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천';
+          글제목변경(copy)
+        }}>바뀜 버튼</button>
         <p>{글날짜[0]}</p>
       </div>
+
       <div className='list'>
         <h4>{글제목[1]}</h4>
         <p>{글날짜[1]}</p>
       </div>
+
       <div className='list'>
         <h4>{글제목[2]}</h4>
         <p>{글날짜[2]}</p>
       </div>
       <h4>{post}</h4>
-    </div>
+    </div >
   );
 }
 
